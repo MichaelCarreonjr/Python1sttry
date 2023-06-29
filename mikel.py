e9b5eb1f03c816219b16ff2fa52d3be0101f1259
MichaelCarreonjr <114980682+MichaelCarreonjr@users.noreply.github.com>
2023-06-29 13:47:56 +0800

    Add files via upload

---
+++ mikel.py
@@ -0,0 +1,14 @@
+num1 =5
+num2 =3
+
+sum =num1+num2
+print("The sum is",format(sum))
+
+sum =num1-num2
+print("The diffrence is" , format(sum))
+
+sum =num1*num2
+print("The Product is",format(sum))
+
+sum =num1/num2
+print("The Quotioent is" , format(sum))
